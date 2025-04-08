4bd0f1bd77f816f30fb1908a53a045259eca340c
Draco <yanlong2944@gmail.com>
2025-04-08 18:19:06 +0800

    🎉 init: init prediction market contract

---
+++ SECURITY.md
@@ -0,0 +1,5 @@
+# 🛡️ Security Policy
+
+## 📬 Reporting a Vulnerability
+
+Please report any security issues you find to [draco@nextmate.ai](mailto:draco@nextmate.ai).
---
+++ arguments.js
@@ -0,0 +1,7 @@
+// 将构造函数导出为参数列表的 JavaScript 模块
+// Hardhat 插件“hardhat-etherscan”所需
+// 另请参阅此处：https://hardhat.org/plugins/nomiclabs-hardhat-etherscan.html#complex-arguments
+
+module.exports = [
+  "Hello, Hardhat!", // 示例 Greeter.sol 文件需要一个字符串
+];
